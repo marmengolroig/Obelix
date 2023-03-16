0c22634a466a7c414576312ee9d04f0ec5d198ce
marmengolroig <marti.armengol.roig@gmail.com>
2023-03-16 13:52:25 +0100

    split .ast files into datablocks

--- main.py
+++ main.py
@@ -7,10 +7,10 @@
 
 path = (
     'Ficheros_asterix/asterix_cat10', 
-    'Ficheros_asterix/201002-lebl-080001_adsb.ast', 
-    'Ficheros_asterix/201002-lebl-080001_mlat.ast',
-    'Ficheros_asterix/201002-lebl-080001_smr.ast',
-    'Ficheros_asterix/201002-lebl-080001_smr_mlat_adsb.ast')
+    'Ficheros_asterix/201002-lebl-080001_adsb.ast', #cat 21
+    'Ficheros_asterix/201002-lebl-080001_mlat.ast', #cat 10
+    'Ficheros_asterix/201002-lebl-080001_smr.ast', #cat 10
+    'Ficheros_asterix/201002-lebl-080001_smr_mlat_adsb.ast') # mixed
 
 decimal_list  = []
 binary_list = []
@@ -20,15 +20,15 @@ byte_list = []
 # A class "data_block" is declared with its specifications: CAT (1 octet) indicates the Category of the data block, LEN (2 octets) 
 # indicates the number of octets of the data block (including CAT and LEN), and REC is the surveillance data of the data block.
 class data_block:
-    def __init__(self,CAT,LEN,REC):
-        self.CAT = CAT
-        self.LEN = LEN
-        self.data = []
+    def __init__(self,cat,len,data):
+        self.cat = cat
+        self.len = len
+        self.data = data
 
 def main():
 
     # open file in binary mode
-    with open(path[0], "rb") as f:
+    with open(path[4], "rb") as f:
         while True: # read byte by byte
             byte = f.read(1) # read 1 byte
             if not byte: 
@@ -42,13 +42,13 @@ def main():
        
 
         # to binary
-        binary_value = bin(decimal_value) # bin() returns the binary value of a decimal
-        binary_value_mod = bin(decimal_value)[2:].zfill(8)
-        binary_list.append(binary_value)
-        binary_mod_list.append(binary_value_mod)
+        # binary_value = bin(decimal_value) # bin() returns the binary value of a decimal
+        # binary_value_mod = bin(decimal_value)[2:].zfill(8)
+        # binary_list.append(binary_value)
+        # binary_mod_list.append(binary_value_mod)
         
     # print(byte_list)    
-    print(decimal_list)
+    # print(decimal_list)
     # print(binary_list)
     # print(binary_mod_list)
     i=0
@@ -59,16 +59,15 @@ def main():
     while i<len(decimal_list):
         if new_db:
             CAT=decimal_list[i]
-            LEN=decimal_list[i+1]+decimal_list[i+2]
+            bin_str=bin(decimal_list[i+1])[2:].zfill(8)+bin(decimal_list[i+2])[2:].zfill(8)
+            LEN=int(bin_str,2)
             data=[]
             data_block_list.append(data_block(CAT,LEN,data))
             sum=sum+LEN
-            k=0
             l=i
-            while k<LEN:
-                data_block_list[n].data.append(data_block_list[l])
+            while l<i+LEN:
+                data_block_list[n].data.append(decimal_list[l])
                 l=l+1
-                k=k+1
             new_db=False
             n=n+1
         if i==sum-1:
@@ -77,10 +76,9 @@ def main():
 
     print("Data blocks: "+str(len(data_block_list)))
     m = 0
-    while m<len(data_block_list):
-        print(data_block_list[m].CAT)
+    while m<3:
+        print(data_block_list[m].data)
         m=m+1
-    print(len(decimal_list))
 
 
 if __name__ == '__main__':
