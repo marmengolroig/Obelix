9f686f425648cf3b683ea2b87eafb8adfe507358
marmengolroig <marti.armengol.roig@gmail.com>
2023-04-26 17:50:45 +0200

    I021/070

--- ClassLibrary/DataItemCat21.py
+++ ClassLibrary/DataItemCat21.py
@@ -14,6 +14,7 @@ from FlightDataItems.I021.I021_075 import I021_075
 from FlightDataItems.I021.I021_140 import I021_140
 from FlightDataItems.I021.I021_090 import I021_090
 from FlightDataItems.I021.I021_210 import I021_210
+from FlightDataItems.I021.I021_070 import I021_070
 
 
 
@@ -55,6 +56,8 @@ class DataItemCat21:
             return I021_090(self)
         elif self.FRN == 18:
             return I021_210(self)
+        elif self.FRN == 19:
+            return I021_070(self)
         
         
     def retrieve_datalist(self):
---
+++ FlightDataItems/I021/I021_070.py
@@ -0,0 +1,29 @@
+# Time of Message Reception for Velocity - I021/070
+# Fixed length: 2 octets
+
+from ClassLibrary.utils import *
+
+class I021_070():
+
+    def __init__(self, parent):
+        self.parent = parent
+        self.parent.ref_no = 'I021/070'
+        self.parent.long = self.set_long()
+        self.parent.length_type = 0 # 0: fixed, 1: extended, 2: repetitive, 3: compound
+        self.parent.dataitem = self
+        self.data = self.set_data()
+        self.decoded_data = self.decode_data()
+
+    def set_long(self):
+        return 2
+    
+    def set_data(self):
+        return self.parent.data_list[0:self.parent.long]
+    
+    def decode_data(self):
+        print(self.data)
+        binary=concatenate_decimals_in_binary(self.data)
+        decimal = int(binary[4:16],2)
+        octal = decimal_to_four_digit_octal(decimal)
+        
+        return (octal)
