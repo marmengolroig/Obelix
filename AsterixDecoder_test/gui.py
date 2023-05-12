d03b87b919140a72e64e1e6c2f12b748f77a7a0c
judithernandez <judithhernandeziribe@gmail.com>
2023-05-12 21:02:39 +0200

    icons in white and other visual changes

--- AsterixDecoder_test/gui.py
+++ AsterixDecoder_test/gui.py
@@ -3,12 +3,33 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
 
+# IMPORT Custom widgets
+from Custom_Widgets.Widgets import *
+
+
+
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        # APPLY JSON STYLESHEET
+        loadJsonStyle(self, self.ui)
         self.show()
+        # EXPAND CENTER MENU WIDGET SIZE
+        self.ui.settingsBtn.clicked.connect(lambda: self.ui.centerMenuContainer.expandMenu())
+        self.ui.helpBtn.clicked.connect(lambda: self.ui.centerMenuContainer.expandMenu())
+        self.ui.infoBtn.clicked.connect(lambda: self.ui.centerMenuContainer.expandMenu())
+        # CLOSE CENTER MENU WIDGET SIZE
+        self.ui.closeCenterMenuBtn.clicked.connect(lambda: self.ui.centerMenuContainer.collapseMenu())
+
+        # EXPAND RIGHT MENU WIDGET SIZE
+        self.ui.moreMenuBtn.clicked.connect(lambda: self.ui.rightMenuContainer.expandMenu())
+        self.ui.profileMenuBtn.clicked.connect(lambda: self.ui.rightMenuContainer.expandMenu())
+        # CLOSE RIGHT MENU WIDGET SIZE
+        self.ui.closeRightMenuBtn.clicked.connect(lambda: self.ui.rightMenuContainer.collapseMenu())
+        
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
